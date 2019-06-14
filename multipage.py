c9e3b6e858e969fd2ad2ff0c37a55a36d7fa8355
alsjakobso <45048582+alsjakobso@users.noreply.github.com>
2019-06-14 12:20:20 -0500

    Add files via upload

---
+++ multipage.py
@@ -0,0 +1,14 @@
+# joins multipage records and their measurements into new, pipe-separated cells and writes to new csv
+
+import pandas as pd
+
+df = pd.read_csv('multi.csv')
+
+condense = df.groupby('record', as_index=False)['file','physical_description_size'].agg('|'.join)
+
+meas = condense['physical_description_size'].str.split('|').apply(set).str.join('|')
+
+condense['meas'] = meas
+
+export_csv = condense.to_csv (r'condensed.csv', index = None, header=True)
+
